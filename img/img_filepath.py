352497054c1f13264efec9cef838e0442e48e0e2
jumpingchu <jumpingchu@gmail.com>
2023-08-02 23:49:29 +0800

    Added ingredients images

--- img/img_filepath.py
+++ img/img_filepath.py
@@ -1,1 +1,19 @@
 POKEMON_SLEEP_IMG = 'img/pokemon_sleep.png'
+
+INGREDIENTS_IMG_LINKS = {
+ '粗枝大蔥': 'https://cdn.kikinote.net/gallery/game/c01528895fc6b17e02f8b54324a51bb1.png',
+ '品鮮蘑菇': 'https://cdn.kikinote.net/gallery/game/8d8fff666ad5e28cfc1e60480c7b620d.png',
+ '特選蛋': 'https://cdn.kikinote.net/gallery/game/c1c2bb1d1ee2d85af65121b5fd59e156.png',
+ '窩心洋芋': 'https://cdn.kikinote.net/gallery/game/98d51c5ddf61576d543375c56e64adcd.png',
+ '特選蘋果': 'https://cdn.kikinote.net/gallery/game/8137e6714bd92715d7f9e07cb04af7b2.png',
+ '火辣香草': 'https://cdn.kikinote.net/gallery/game/c44560973b8e6eff968442bb33748a6a.png',
+ '豆製肉': 'https://cdn.kikinote.net/gallery/game/6b11c40a1d33e139444e9c536929a31b.png',
+ '哞哞鮮奶': 'https://cdn.kikinote.net/gallery/game/b88926ef0f9678b1c90359a2e31840e2.png',
+ '甜甜蜜': 'https://cdn.kikinote.net/gallery/game/9f63ac0be11bac68abb0ad29e6a60192.png',
+ '純粹油': 'https://cdn.kikinote.net/gallery/game/4b0c122d21f98eb39540362baad93541.png',
+ '暖暖薑': 'https://cdn.kikinote.net/gallery/game/7ceb757a54887ac91a607a486919bc55.png',
+ '好眠番茄': 'https://cdn.kikinote.net/gallery/game/b4fb74d1be02ed20ad35bd4697af5171.png',
+ '放鬆可可': 'https://cdn.kikinote.net/gallery/game/53c426a02bab0101d7c8f448151453f5.png',
+ '美味尾巴': 'https://cdn.kikinote.net/gallery/game/3d6b933ed6528aedc942ffedb87ad9bd.png',
+ '萌綠大豆': 'https://cdn.kikinote.net/gallery/game/d6eefc6bd5ea03f15960a977abc065f0.png'
+}
