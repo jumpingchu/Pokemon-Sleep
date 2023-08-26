9cac9734fb5b7e857cf49f1fe2db2ba0b14b539f
jumpingchu <jumpingchu@gmail.com>
2023-08-26 17:46:44 +0800

    Add tesseract packages

--- pages/3_寶可夢能力計算機.py
+++ pages/3_寶可夢能力計算機.py
@@ -7,7 +7,7 @@ from img_util.parse_img import TransformImage
 st.set_page_config(page_title='Pokemon Sleep App', layout="wide")
 st.title('Pokemon Sleep 寶可夢能力計算機')
 st.caption('上傳寶可夢的截圖，自動取得技能和性格等資訊')
-st.warning('施工中', icon="⚠️")
+st.warning('目前僅能辨識圖片，尚無計算功能', icon="⚠️")
 
 uploaded_file = st.file_uploader("上傳寶可夢截圖", type=['jpg', 'png'])
 st.divider()
@@ -21,5 +21,5 @@ if uploaded_file is not None:
     with info_col:
         st.dataframe(df_text)
 else:
-    st.write('截圖範例（左上角寶可夢方框剛好遮住第一個樹果）')
-    st.image('img/test3.PNG', width=600)
+    st.write('截圖範例（左上角寶可夢方框剛好遮住第一個食材）')
+    st.image('img/test3.PNG', width=200)
