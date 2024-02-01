70a7c5d2454bd5fcc537362f30dc9c3c946c1446
jumpingchu <jumpingchu@gmail.com>
2024-02-01 19:27:12 +0800

    Refactor: change DB from Mongo to BigQuery

---
+++ pages/3_潛力計算機 copy.py
@@ -0,0 +1,142 @@
+import os
+import streamlit as st
+from PIL import Image
+from google.cloud import bigquery as bq
+from google.oauth2 import service_account
+
+st.set_page_config(page_title="Pokemon Sleep App", layout="wide")
+st.title("Pokemon Sleep 寶可夢潛力計算機")
+st.caption("上傳寶可夢的截圖，自動取得圖片資訊，並可一鍵計算潛力評價")
+st.caption("若覺得有幫助到你的話，歡迎小額支持我: https://www.buymeacoffee.com/jiapingchu")
+
+uploaded_file = st.file_uploader("上傳截圖", type=["jpg", "png"])
+st.divider()
+
+if uploaded_file is not None:
+    with st.status("圖片上傳中...") as status:
+        from pages.util.util import process_img
+        img = uploaded_file.getvalue()
+        status.update(label="辨識圖片中...", state="running")
+        info = process_img(img)
+        print(info)
+        status.update(label="圖片辨識完成！", state="complete")
+
+    # 顯示圖片
+    # st.header('上傳的圖片')
+    st.image(Image.open(uploaded_file))
+
+    # 顯示擷取結果到文字輸入框
+    st.header("圖片辨識結果")
+
+    with st.form("my_form"):
+
+        if info["pokemon"]:
+            from pages.util.util import (
+                get_pokemon_info_from_bq,
+                get_rank_color_text,
+                get_item_list_from_bq,
+                get_nature_dict_from_bq,
+                get_ingredient_dict_from_bq,
+            )
+            from google.cloud import bigquery as bq
+
+            credentials = service_account.Credentials.from_service_account_info(
+                st.secrets["gcp_service_account"]
+            )
+            client = bq.Client(credentials=credentials)
+
+            # Pokemon
+            pokemon_info = get_pokemon_info_from_bq(info["pokemon"])
+            show_info = {
+                'name': '寶可夢',
+                'fruit': '樹果',
+                'ingredient': '食材 1',
+                'main_skill': '主技能',
+            }
+            for k, v in pokemon_info.items():
+                if k in show_info.keys():
+                    k_zhtw = show_info.get(k)
+                    st.markdown(f'- **{k_zhtw}**: {v}')
+
+            # Sub Skills
+            sub_skills_list = get_item_list_from_bq("SubSkill")
+            sub_skills_list = sorted(sub_skills_list)
+            sub_skills_list.insert(0, "---")
+            try:
+                sub_skill_1 = st.text_input("副技能1", value=f"{info['sub_skill_1']}")
+            except:
+                sub_skill_1 = st.selectbox(":orange[副技能1]", sub_skills_list)
+            try:
+                sub_skill_2 = st.text_input("副技能2", value=f"{info['sub_skill_2']}")
+            except:
+                sub_skill_2 = st.selectbox(":orange[副技能2]", sub_skills_list)
+            try:
+                sub_skill_3 = st.text_input("副技能3", value=f"{info['sub_skill_3']}")
+            except:
+                sub_skill_3 = st.selectbox(":orange[副技能3]", sub_skills_list)
+            try:
+                sub_skill_4 = st.text_input("副技能4", value=f"{info['sub_skill_4']}")
+            except:
+                sub_skill_4 = st.selectbox(":orange[副技能4]", sub_skills_list)
+            try:
+                sub_skill_5 = st.text_input("副技能5", value=f"{info['sub_skill_5']}")
+            except:
+                sub_skill_5 = st.selectbox(":orange[副技能5]", sub_skills_list)
+
+            sub_skills = [sub_skill_1, sub_skill_2, sub_skill_3, sub_skill_4, sub_skill_5]
+
+            # Ingredient 2 and 3
+            ingredient_list = get_item_list_from_bq("Ingredient")
+            ingredient_list.insert(0, "---")
+            ingredient_2 = st.selectbox(
+                ":orange[食材2]", ingredient_list, index=ingredient_list.index(pokemon_info['ingredient'])
+            )
+            ingredient_2_num = st.slider(
+                ":orange[食材2數量]", value=2, min_value=1, max_value=10, step=1
+            )
+
+            ingredient_3 = st.selectbox(
+                ":orange[食材3]", ingredient_list, index=ingredient_list.index(pokemon_info['ingredient'])
+            )
+            ingredient_3_num = st.slider(
+                ":orange[食材3數量]", value=4, min_value=1, max_value=10, step=1
+            )
+
+            # Nature
+            nature = st.text_input("性格", value=f"{info.get('nature', '')}")
+            if nature:
+                nature_data = get_nature_dict_from_bq(nature)
+                nature_up = nature_data["up"] if nature_data["up"] else '無性格效果'
+                nature_down = nature_data["down"] if nature_data["down"] else '無性格效果'
+                st.write(":small_red_triangle: UP: ", nature_up)
+                st.write(":small_blue_diamond: DOWN: ", nature_down)
+                submitted = st.form_submit_button("計算能力")
+
+            if submitted:
+                with st.status("計算中...") as status:
+                    from img_util.calculator import calculator
+                    ingredient_2_energy = get_ingredient_dict_from_bq(ingredient_2)['energy']
+                    ingredient_3_energy = get_ingredient_dict_from_bq(ingredient_3)['energy']
+                    info_dict = {
+                        'pokemon_info': pokemon_info,
+                        'nature_up': nature_up,
+                        'nature_down': nature_down,
+                        'sub_skills': sub_skills,
+                        'ingredient_2_num': int(ingredient_2_num),
+                        'ingredient_2_energy': int(ingredient_2_energy),
+                        'ingredient_3_num': int(ingredient_3_num),
+                        'ingredient_3_energy': int(ingredient_3_energy),
+                    }
+
+                    energy_score, rank = calculator(**info_dict)
+                    status.update(label="計算完成！", state="complete", expanded=True)
+
+                    st.header(f"能量積分: :blue[{energy_score}]")
+                    st.header(get_rank_color_text(rank))
+        else:
+            print('無法辨識寶可夢名稱！請更換圖片後再試試看～')
+
+else:
+    st.header("截圖範例")
+    st.write("左上角寶可夢方框剛好「遮住第一個食材」，並且最底部剛好出現「性格」")
+    st.image("img/test1.PNG")
--- pages/util/util.py
+++ pages/util/util.py
@@ -1,119 +1,253 @@
+import os
 import pandas as pd
 import numpy as np
 import streamlit as st
+from google.cloud import bigquery as bq
+from google.oauth2 import service_account
+from img_util.parse_img import TransformImage
 
-category_list = ['全部', '點心/飲料', '沙拉', '咖哩/濃湯']
+# Create API client.
+
+category_list = ["全部", "點心/飲料", "沙拉", "咖哩/濃湯"]
 curry_soup_list = [
-    '特選蘋果咖哩',
-    '炙燒尾肉咖哩',
-    '太陽之力番茄咖哩',
-    '絕對睡眠奶油咖哩',
-    '辣味蔥勁十足咖哩',
-    '蘑菇孢子咖哩',
-    '親子愛咖哩',
-    '吃飽飽起司肉排咖哩',
-    '窩心白醬濃湯',
-    '單純白醬濃湯',
-    '豆製肉排咖哩',
-    '寶寶甜蜜咖哩',
-    '忍者咖哩',
-    '日照炸肉排咖哩',
-    '入口即化蛋捲咖哩',
-    '健美豆子咖哩',
+    "特選蘋果咖哩",
+    "炙燒尾肉咖哩",
+    "太陽之力番茄咖哩",
+    "絕對睡眠奶油咖哩",
+    "辣味蔥勁十足咖哩",
+    "蘑菇孢子咖哩",
+    "親子愛咖哩",
+    "吃飽飽起司肉排咖哩",
+    "窩心白醬濃湯",
+    "單純白醬濃湯",
+    "豆製肉排咖哩",
+    "寶寶甜蜜咖哩",
+    "忍者咖哩",
+    "日照炸肉排咖哩",
+    "入口即化蛋捲咖哩",
+    "健美豆子咖哩",
 ]
 salad_list = [
-    '呆呆獸尾巴的胡椒沙拉',
-    '蘑菇孢子沙拉',
-    '撥雪凱撒沙拉',
-    '貪吃鬼洋芋沙拉',
-    '濕潤豆腐沙拉',
-    '蠻力豪邁沙拉',
-    '豆製火腿沙拉',
-    '好眠番茄沙拉',
-    '哞哞起司番茄沙拉',
-    '心情不定肉沙拉淋巧克力醬',
-    '過熱沙拉',
-    '特選蘋果沙拉',
-    '免疫蔥花沙拉',
-    '迷人蘋果起司沙拉',
-    '忍者沙拉',
-    '熱風豆腐沙拉',
+    "呆呆獸尾巴的胡椒沙拉",
+    "蘑菇孢子沙拉",
+    "撥雪凱撒沙拉",
+    "貪吃鬼洋芋沙拉",
+    "濕潤豆腐沙拉",
+    "蠻力豪邁沙拉",
+    "豆製火腿沙拉",
+    "好眠番茄沙拉",
+    "哞哞起司番茄沙拉",
+    "心情不定肉沙拉淋巧克力醬",
+    "過熱沙拉",
+    "特選蘋果沙拉",
+    "免疫蔥花沙拉",
+    "迷人蘋果起司沙拉",
+    "忍者沙拉",
+    "熱風豆腐沙拉",
 ]
 snack_drink_list = [
-    '熟成甜薯燒',
-    '不屈薑餅',
-    '特選蘋果汁',
-    '手製勁爽汽水',
-    '火花薑茶',
-    '胖丁百匯布丁',
-    '惡魔之吻水果牛奶',
-    '祈願蘋果派',
-    '橙夢的排毒茶',
-    '甜甜香氣巧克力蛋糕',
-    '哞哞熱鮮奶',
-    '輕裝豆香蛋糕',
-    '活力蛋白飲',
-    '我行我素蔬菜汁',
-    '大馬拉薩達',
-    '大力士豆香甜甜圈',
+    "熟成甜薯燒",
+    "不屈薑餅",
+    "特選蘋果汁",
+    "手製勁爽汽水",
+    "火花薑茶",
+    "胖丁百匯布丁",
+    "惡魔之吻水果牛奶",
+    "祈願蘋果派",
+    "橙夢的排毒茶",
+    "甜甜香氣巧克力蛋糕",
+    "哞哞熱鮮奶",
+    "輕裝豆香蛋糕",
+    "活力蛋白飲",
+    "我行我素蔬菜汁",
+    "大馬拉薩達",
+    "大力士豆香甜甜圈",
 ]
 
 all_recipe_dict = {
-    '全部': ['全部'] + curry_soup_list + salad_list + snack_drink_list,
-    '點心/飲料': ['全部'] + snack_drink_list,
-    '沙拉': ['全部'] + salad_list,
-    '咖哩/濃湯': ['全部'] + curry_soup_list
+    "全部": ["全部"] + curry_soup_list + salad_list + snack_drink_list,
+    "點心/飲料": ["全部"] + snack_drink_list,
+    "沙拉": ["全部"] + salad_list,
+    "咖哩/濃湯": ["全部"] + curry_soup_list,
 }
 
 show_cols = [
-    '食譜',
+    "食譜",
     # '分類',
     # '食材1圖示',
-    '食材1數量', 
+    "食材1數量",
     # '食材2圖示',
-    '食材2數量', 
+    "食材2數量",
     # '食材3圖示',
-    '食材3數量', 
+    "食材3數量",
     # '食材4圖示',
-    '食材4數量',
+    "食材4數量",
 ]
+
 
 def get_ingredient_unique_list(df):
     ingredient_list = [
-        *df['食材1'], 
-        *df['食材2'],
-        *df['食材3'],
-        *df['食材4'],
+        *df["食材1"],
+        *df["食材2"],
+        *df["食材3"],
+        *df["食材4"],
     ]
     ingredient_unique_list = list(set(ingredient_list))
     ingredient_unique_list = [i for i in ingredient_unique_list if i is not np.nan]
     return ingredient_unique_list
 
+
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600) # Time to Live = 600 seconds
+@st.cache_data(ttl=600)  # Time to Live = 600 seconds
 def load_gsheet_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url, on_bad_lines='skip')
+    return pd.read_csv(csv_url, on_bad_lines="skip")
+
 
 @st.cache_data
 def get_can_cook(df, have_ingredients, match_mode):
     if not have_ingredients:
         return df
-    
+
     index_match = []
     for row in df.itertuples():
-        if match_mode == '任一食材符合':
+        if match_mode == "任一食材符合":
             if any(i in row.all_food for i in have_ingredients):
                 index_match.append(row.Index)
         else:
             if all(i in row.all_food for i in have_ingredients):
                 index_match.append(row.Index)
-                
+
     can_cook = df.iloc[index_match]
     return can_cook
 
+
 def filter_category(df, category):
-    return df.query(f"分類 == '{category}'") if category != '全部' else df
+    return df.query(f"分類 == '{category}'") if category != "全部" else df
+
 
 def filter_recipe(df, recipe):
-    return df.query(f"食譜 == '{recipe}'") if recipe != '全部' else df
+    return df.query(f"食譜 == '{recipe}'") if recipe != "全部" else df
+
+
+@st.cache_data
+def get_pokemon_info_from_bq(pokemon):
+    sql = f"""
+        SELECT
+            p.* EXCEPT (_airbyte_raw_id,
+                _airbyte_extracted_at,
+                _airbyte_meta),
+            i.name AS ingredient,
+            i.energy AS ingredient_energy,
+            f.name AS fruit,
+            f.lv60_energy AS fruit_energy,
+            m.name AS main_skill,
+            m.Lv1,
+            m.Lv2,
+            m.Lv3,
+            m.Lv4,
+            m.Lv5,
+            m.Lv6,
+        FROM
+            `PokemonSleep.Pokemon` AS p
+        JOIN
+            `PokemonSleep.Ingredient` AS i
+        ON
+            i.name = p.ingredient
+        JOIN
+            `PokemonSleep.Fruit` AS f
+        ON
+            f.name = p.fruit
+        JOIN
+            `PokemonSleep.MainSkill` AS m
+        ON
+            m.name = p.main_skill
+        WHERE p.name = '{pokemon}'
+    """
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+    client = bq.Client(credentials=credentials)
+    query_job = client.query(sql)
+    result_dict = [dict(result) for result in query_job][0]
+    return result_dict
+
+
+@st.cache_data
+def get_item_list_from_bq(table_name):
+    sql = f"""
+        SELECT
+            DISTINCT(name)
+        FROM
+            `PokemonSleep.{table_name}`
+    """
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+    client = bq.Client(credentials=credentials)
+    query_job = client.query(sql)
+    result_list = [result.values()[0] for result in query_job]
+    return result_list
+
+
+@st.cache_data
+def get_nature_dict_from_bq(nature_name):
+    sql = f"""
+        SELECT
+            name,
+            up,
+            down,
+        FROM
+            `PokemonSleep.Nature`
+        WHERE name = '{nature_name}'
+    """
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+    client = bq.Client(credentials=credentials)
+    query_job = client.query(sql)
+    result_dict = [dict(result) for result in query_job][0]
+    return result_dict
+
+@st.cache_data
+def get_ingredient_dict_from_bq(ingredient):
+    sql = f"""
+        SELECT
+            name,
+            energy,
+        FROM
+            `PokemonSleep.Ingredient`
+        WHERE name = '{ingredient}'
+    """
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+    client = bq.Client(credentials=credentials)
+    query_job = client.query(sql)
+    result_dict = [dict(result) for result in query_job][0]
+    return result_dict
+
+def get_rank_color_text(rank):
+    rank_color_dict = {
+        "SSS": f"評價: :rainbow[{rank}]",
+        "SS": f"評價: :rainbow[{rank}]",
+        "S": f"評價: :rainbow[{rank}]",
+        "A": f"評價: :red[{rank}]",
+        "B": f"評價: :violet[{rank}]",
+        "C": f"評價: :blue[{rank}]",
+        "D": f"評價: {rank}",
+        "E": f"評價: {rank}",
+    }
+    return rank_color_dict.get(rank)
+
+
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = st.secrets['bq_credentials_filepath']
+# client = bq.Client()
+# get_nature_dict_from_bq(client, '認真')
+
+
+@st.cache_data(max_entries=3)
+def process_img(img):
+    transform_img = TransformImage(img)
+    info = transform_img.run()
+    return info
